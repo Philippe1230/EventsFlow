b21c7c02696cdab598fadf4d98dc6481d018ab0b
PrintCalc Pro - Suporte <printcalcprosuporte@gmail.com>
2026-05-01 01:54:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5012486883-b0506",
+  "appId": "1:842413718154:web:2e6bdc2a49506600b88b73",
+  "apiKey": "AIzaSyC6SsKgN25Ysg4yNEQMAQyR-Medeux5Jds",
+  "authDomain": "studio-5012486883-b0506.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "842413718154"
+};
